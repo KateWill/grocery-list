5942969cfe01921101aa067cef9b6889929d86f1
Katelyn Williamson <katelynrwilliamson@gmail.com>
2019-03-21 20:46:18 -0600

    added form component

--- src/App.js
+++ src/App.js
@@ -1,21 +1,25 @@
 import React, { Component } from 'react';
 import List from './grocery/List';
-
+import AddForm from './grocery/AddForm'
 
 class App extends Component {
   state = {
     list: [ 
-      {id: 1, name: 'apple', price: 1.00},
-      {id: 2, name: 'toothpaste', price: 3.00},
-      {id: 3, name: 'candy', price: 5.00},
+      {id: 1, itemName: 'apple', price: 1.00},
+      {id: 2, itemName: 'toothpaste', price: 3.00},
+      {id: 3, itemName: 'candy', price: 5.00},
     ]
   }
 
+  addItem = (incomingListItem) =>{
+
+  }
+
   render() {
-    const { list } = this.state
+    const { list, addItem } = this.state
     return (
       <div>
-        {/* <h1>Grocery List</h1> */}
+        <AddForm addItem={addItem} />
         <List name="Grocery List" items={list} />
       </div>
     );
--- src/grocery/AddForm.js
+++ src/grocery/AddForm.js
@@ -1,7 +1,41 @@
 import React from 'react'
 
 class AddForm extends React.Component {
+    state = { itemName: '', price: 0.00 }
+    
+    handleSubmit = (e) =>{
+      e.preventDefault()
+      this.props.addItem(this.state.name)
+      this.setState( {itemName: '', price: 0.00 })
+    }
 
+    handleChange = (e) =>{
+      const {name, value} = e.target
+      this.setState({ [name]: value })
+    }
+    render(){
+      const { itemName, price } = this.state
+      return(
+        <form onSubmit={this.handleSubmit}>
+        <input 
+          placeholder="Add List Item"
+          value={itemName}
+          name="itemName"
+          onChange={this.handleChange}
+        />
+        <br/>       
+        <input 
+          placeholder="Add List Item"
+          value={price}
+          name="price"
+          type="number"
+          onChange={this.handleChange}
+        />
+        <br/>
+        <button>Add to list</button>
+      </form>
+      )
+    }
 }
 
 export default AddForm
